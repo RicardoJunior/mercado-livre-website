bc7e01dc30cfbb4629f7c7619ba47db2b465469d
Ricardo Junior <eu@ricardojr.com.br>
2017-07-21 17:34:52 -0300

    Add routes, add masterpage style

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,27 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import { injectGlobal, ThemeProvider } from 'styled-components'
-
-import { HomePage } from 'components'
-
-import theme from './themes/default'
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { injectGlobal, ThemeProvider } from 'styled-components';
+import { HomePage, SearchListDetail } from 'components';
+import theme from './themes/default';
 
 injectGlobal`
   body {
     margin: 0;
+    padding: 0;
+    background-color: #f5f5f5;
   }
-`
+`;
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Switch>
         <Route path="/" component={HomePage} exact />
+        <Route path="/items" component={HomePage} exact />
+        <Route path="/items/:id/:desc" component={SearchListDetail} exact />
       </Switch>
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
