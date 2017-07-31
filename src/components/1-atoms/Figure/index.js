a714db29a6ad2a042be13f90351a2a239c01bfe2
Ricardo Junior <eu@ricardojr.com.br>
2017-07-31 17:43:03 -0300

    Add figure project in git

--- src/components/1-atoms/Figure/index.js
+++ src/components/1-atoms/Figure/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
 
-const Figure = ({ logo, title, ...props }) => (<figure>
+const styles = css`
+  padding: 0;
+  margin: 0;
+  display: block;
+
+  img {
+    display: block;
+    vertical-align: top;
+  }
+`;
+
+const StyledFigure = styled.figure`${styles}`;
+
+const Figure = ({ logo, title, ...props }) => (<StyledFigure>
   <img src={logo} {...props} alt={title} />
   <figcaption>{title}</figcaption>
-</figure>);
+</StyledFigure>);
 
 Figure.propTypes = {
   logo: PropTypes.string,
